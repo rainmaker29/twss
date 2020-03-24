6a1aed7576cd05297d5e6c9b215efbe484aa209c
rainmaker29 <amaanrahil29@gmail.com>
2020-03-24 23:17:51 +0530

    app update

--- app.py
+++ app.py
@@ -64,5 +64,9 @@ def results():
     output = prediction[0]
     return jsonify(output)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":]
+    app.run(host='0.0.0.0')
+
+
+
+
